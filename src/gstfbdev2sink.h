7c5ca070ea5fe2829a88de212a10330f1d7bcecb
Harm Hanemaaijer <fgenfb@yahoo.com>
2013-08-08 17:03:15 +0200

    Fix typo in email address

--- src/gstfbdev2sink.h
+++ src/gstfbdev2sink.h
@@ -1,5 +1,5 @@
 /* GStreamer fbdev2sink plugin
- * Copyright (C) 2013 Harm Hanemaaijer <fgenfg@yahoo.com>
+ * Copyright (C) 2013 Harm Hanemaaijer <fgenfb@yahoo.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
--- src/gstsunxifbsink.c
+++ src/gstsunxifbsink.c
@@ -1,5 +1,5 @@
 /* GStreamer sunxifbsink plugin
- * Copyright (C) 2013 Harm Hanemaaijer <fgenfg@yahoo.com>
+ * Copyright (C) 2013 Harm Hanemaaijer <fgenfb@yahoo.com>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
